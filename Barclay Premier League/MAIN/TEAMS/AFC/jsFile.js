2cc6a85523a5c5065059656a5149ecf5a58de736
Kyaw Kyaw Aung <51185339+kyawkyawaung2019@users.noreply.github.com>
2020-04-19 20:49:56 +0630

    Add files via upload

---
+++ Barclay Premier League/MAIN/TEAMS/AFC/jsFile.js
@@ -0,0 +1,39 @@
+﻿function HOME(){ 
+	var HOME = document.getElementById("HOME");
+	var fun = function(){
+		alert("HOME page မွ ၾကိဳဆိုပါသည္...\nသာယာေပ်ာ္ရႊင္ဖြယ္ အခ်ိန္ေလးျဖစ္ပါေစ...");
+	};
+ 	EventUtil.addHandler(HOME, "click",  fun);
+}
+
+function PHOTOS(){
+	var PHOTOS = document.getElementById("PHOTOS");
+	var fun = function(){
+		alert("PHOTOS page မွ ၾကိဳဆိုပါသည္...\nသာယာေပ်ာ္ရႊင္ဖြယ္ အခ်ိန္ေလးျဖစ္ပါေစ...");
+	};
+	EventUtil.addHandler(PHOTOS, "click", fun);
+}
+
+function VIDEOS(){
+	var VIDEOS = document.getElementById("VIDEOS");
+	var fun = function(){
+		alert("VIDEOS page မွ ၾကိဳဆိုပါသည္...\nသာယာေပ်ာ္ရႊင္ဖြယ္ အခ်ိန္ေလးျဖစ္ပါေစ...");
+	};
+	EventUtil.addHandler(VIDEOS, "click", fun);
+}
+
+function ABOUT(){
+	var ABOUT = document.getElementById("ABOUT");
+	var fun = function(){
+		alert("ABOUT page မွ ၾကိဳဆိုပါသည္...\nသာယာေပ်ာ္ရႊင္ဖြယ္ အခ်ိန္ေလးျဖစ္ပါေစ...");
+	};
+	EventUtil.addHandler(ABOUT, "click", fun);
+}
+
+function LOGOUT(){
+	var LOGOUT = document.getElementById("LOGOUT");
+	var fun = function(){
+		alert("Bye! \nBarclay Premier League Page မွ နွဳတ္ဆက္ပါသည္။\nသာယာေပ်ာ္ရႊင္ဖြယ္ အခ်ိန္ေလးေတြကို ပိုင္ဆိုင္နိဳင္ပါေစ..");
+	};
+	EventUtil.addHandler(LOGOUT, "click", fun);
+}
