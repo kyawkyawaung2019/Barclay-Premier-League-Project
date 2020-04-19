71ede960d01c5acd2607401eeb908f5a4f54e463
Kyaw Kyaw Aung <51185339+kyawkyawaung2019@users.noreply.github.com>
2020-04-19 20:02:35 +0630

    Add files via upload

---
+++ Barclay Premier League/MAIN/ABOUT/ABOUT.js
@@ -0,0 +1,108 @@
+﻿function HOME(){ 
+	var HOME = document.getElementById("HOME");
+	var fun = function(){
+		alert("HOME page မွ ၾကိဳဆိုပါသည္...\nသာယာေပ်ာ္ရႊင္ဖြယ္ အခ်ိန္ေလးျဖစ္ပါေစ...");
+	};
+ 	EventUtil.addHandler(HOME, "click",  fun);
+}
+
+function PHOTOS(){
+	var PHOTOS = document.getElementById("PHOTOS");
+	var fun = function(){
+		alert("PHOTOS page မွ ၾကိဳဆိုပါသည္...\nသာယာေပ်ာ္ရႊင္ဖြယ္ အခ်ိန္ေလးျဖစ္ပါေစ...");
+	};
+	EventUtil.addHandler(PHOTOS, "click", fun);
+}
+
+function VIDEOS(){
+	var VIDEOS = document.getElementById("VIDEOS");
+	var fun = function(){
+		alert("VIDEOS page မွ ၾကိဳဆိုပါသည္...\nသာယာေပ်ာ္ရႊင္ဖြယ္ အခ်ိန္ေလးျဖစ္ပါေစ...");
+	};
+	EventUtil.addHandler(VIDEOS, "click", fun);
+}
+
+function LOGOUT(){
+	var LOGOUT = document.getElementById("LOGOUT");
+	var fun = function(){
+		alert("Bye! \nBarclay Premier League Page မွ နွဳတ္ဆက္ပါသည္။\nသာယာေပ်ာ္ရႊင္ဖြယ္ အခ်ိန္ေလးေတြကို ပိုင္ဆိုင္နိဳင္ပါေစ..");
+	};
+	EventUtil.addHandler(LOGOUT, "click", fun);
+}
+
+
+
+
+
+function AungPhyoThu(){
+	var name = document.getElementById("APT");
+	var fun = function(){
+		alert("Name : Aung Phyo Thu"+
+			"\nStudent ID : 2CS-38"+
+			"\n2013-2014 ပညာသင္နွစ္တြင္ ကြန္ပ် ဴ တာ တကၠသိုလ္ (မအူပင္) ၌ ပညာမ်ားကို စတင္ ေလ့လာသင္ယူသည္။");
+		};
+	EventUtil.addHandler(name, "click", fun);
+}
+function AyeChanAung(){
+	var name = document.getElementById("ACA");
+	var fun = function(){
+		alert("Name : Aye Chan Aung"+
+			"\nStudent ID : 2CS-31"+
+			"\n2014-2015 ပညာသင္နွစ္တြင္ ကြန္ပ် ဴ တာ တကၠသိုလ္ (မအူပင္) ၌ ပညာမ်ားကို စတင္ ေလ့လာသင္ယူသည္။");
+		};
+	EventUtil.addHandler(name, "click", fun);
+}
+function HeinHtetZaw(){
+	var name = document.getElementById("HHZ");
+	var fun = function(){
+		alert("Name : Hein Htet Zaw"+
+			"\nStudent ID : 2CS-35"+
+			"\n2013-2014 ပညာသင္နွစ္တြင္ ကြန္ပ် ဴ တာ တကၠသိုလ္ (မအူပင္) ၌ ပညာမ်ားကို စတင္ ေလ့လာသင္ယူသည္။");
+		};
+	EventUtil.addHandler(name, "click", fun);
+}
+function HeinMinSan(){
+	var name = document.getElementById("HMS");
+	var fun = function(){
+		alert("Name : Hein Min San"+
+			"\nStudent ID : 2CS-43"+
+			"\n2014-2015 ပညာသင္နွစ္တြင္ ကြန္ပ် ဴ တာ တကၠသိုလ္ (မအူပင္) ၌ ပညာမ်ားကို စတင္ ေလ့လာသင္ယူသည္။"); 
+		};
+	EventUtil.addHandler(name, "click", fun);
+}
+function KyawKyawAung(){
+	var name = document.getElementById("KKA");
+	var fun = function(){
+		alert("Name : Kyaw Kyaw Aung"+
+			"\nStudent ID : 2CS-6"+
+			"\n2013-2014 ပညာသင္နွစ္တြင္ ကြန္ပ် ဴ တာ တကၠသိုလ္ (မအူပင္) ၌ ပညာမ်ားကို စတင္ ေလ့လာသင္ယူသည္။");
+		};
+	EventUtil.addHandler(name, "click", fun);
+}
+function YanNaingSoe(){
+	var name = document.getElementById("YNS");
+	var fun = function(){
+		alert("Name : Yan Naing Soe"+
+			"\nStudent ID : 2CS-5"+
+			"\n2014-2015 ပညာသင္နွစ္တြင္ ကြန္ပ် ဴ တာ တကၠသိုလ္ (မအူပင္) ၌ ပညာမ်ားကို စတင္ ေလ့လာသင္ယူသည္။");
+		};
+	EventUtil.addHandler(name, "click", fun);
+}
+function YeKoHlaing(){
+	var name = document.getElementById("YKH");
+	var fun = function(){
+		alert("Name : Ye Ko Hlaing"+
+			"\nStudent ID : 2CS-41"+
+			"\n2014-2015 ပညာသင္နွစ္တြင္ ကြန္ပ် ဴ တာ တကၠသိုလ္ (မအူပင္) ၌ ပညာမ်ားကို စတင္ ေလ့လာသင္ယူသည္။");
+		};
+	EventUtil.addHandler(name, "click", fun);
+}
+function YeYintAung(){
+	var name = document.getElementById("YYA");
+	var fun = function(){
+		alert("Name : Ye Yint Aung"+
+			"\nStudent ID : 2CS-25"+
+			"\n2014-2015 ပညာသင္နွစ္တြင္ ကြန္ပ် ဴ တာ တကၠသိုလ္ (မအူပင္) ၌ ပညာမ်ားကို စတင္ ေလ့လာသင္ယူသည္။");
+		};
+	EventUtil.addHandler(name, "click", fun);
+}
